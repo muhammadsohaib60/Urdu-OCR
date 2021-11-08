a6ba35cd8bfa7f4583100530cf1ba4d71e33e03b
Muhammad Sohaib <58735769+muhammadsohaib60@users.noreply.github.com>
2021-11-08 14:43:09 +0500

    Delete code.py

--- code.py
+++
@@ -1,9 +0,0 @@
-Run the below code to read and display image using OpenCV in google collab,
-import cv2
-import numpy as np
-from google.colab.patches import cv2_imshow
-
-img=cv2.imread("TajMahal.JPG")
-
-cv2_imshow(img)
-
